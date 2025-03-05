f9c4468e6b74f96f185c401b3e05afd31d344930
BM <Bartlomiej.Madura@Honeywell.com>
2025-03-05 14:20:37 +0100

    Refactoring to structs

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,36 @@ fn main(){
     // Read the function args and parse them to variables
     let args: Vec<String> = env::args().collect();
     
-    let (query, file_path) = parse_config(&args);
+    let config: Config = Config::new(&args);
 
     // Read the target file
-    let file_content: String = fs::read_to_string(file_path)
+    let file_content: String = fs::read_to_string(&config.file_path)
         .expect("Failed to read the file");
 
-    println!("In file: {}",file_path);
+    println!("In file: {}",config.file_path);
     println!("{}",file_content);
 
 }
-
-fn parse_config(args: &[String]) -> (&str, &str){
-
-    let query: &String = &args[1];
-    let file_path: &String = &args[2];
-
-    (query, file_path)
-
+struct Config {
+    query: String,
+    file_path: String,
 }
+
+impl Config{
+    fn new(args: &[String]) -> Config{
+    
+        if args.len() < 3 {
+            panic!("Not enough arguments");
+        }
+
+        if args.len() > 3{
+            // ToDo! - print to stderr
+            pritnln!("Ignoring extra arguments passed (>2)");
+        }
+
+        Config{query: args[1].clone(),
+               file_path: args[2].clone()} 
+
+    }
+
+    }

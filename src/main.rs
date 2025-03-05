9f206279eaec6fdf918530a843ce25d648502fd5
BM <Bartlomiej.Madura@Honeywell.com>
2025-03-05 14:32:59 +0100

    Improved error handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main(){
     // Read the function args and parse them to variables
     let args: Vec<String> = env::args().collect();
     
-    let config: Config = Config::new(&args);
+    let config: Config = Config::build(&args).unwrap_or_else(|err: &str| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     // Read the target file
     let file_content: String = fs::read_to_string(&config.file_path)
@@ -21,19 +25,19 @@ struct Config {
 }
 
 impl Config{
-    fn new(args: &[String]) -> Config{
+    fn build(args: &[String]) -> Result<Config, &'static str>{
     
         if args.len() < 3 {
-            panic!("Not enough arguments");
+            return Err("Not enough arguments");
         }
 
         if args.len() > 3{
-            // ToDo! - print to stderr
-            pritnln!("Ignoring extra arguments passed (>2)");
+            // ToDo! - print warning (to stderr?) and continue
+            println!("Ignoring extra arguments passed (>2)");
         }
 
-        Config{query: args[1].clone(),
-               file_path: args[2].clone()} 
+        Ok(Config{query: args[1].clone(),
+               file_path: args[2].clone()})
 
     }
 
